f7fa9d0df671c78283269a45da58b74264ade5d2
mathieu-Bro <57324777+mathieu-Bro@users.noreply.github.com>
2026-05-16 08:45:27 +0200

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", async function () {
-  await loadLayout();
-  await loadGalleryData();
-
-  initFilters();
-  populateFolderFilter();
-  applyFiltersAndRender();
-});
